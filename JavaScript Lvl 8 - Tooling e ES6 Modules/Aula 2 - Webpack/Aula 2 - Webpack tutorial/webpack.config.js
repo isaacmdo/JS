55315eead5c1ff315cd094f761b7b0b5b0d73f7a
Isaac Moura de Oliveira <isaacestudo1999@gmail.com>
2020-12-29 09:15:16 -0500

    Update webpack.config.js

--- JavaScript Lvl 8 - Tooling e ES6 Modules/Aula 2 - Webpack/Aula 2 - Webpack tutorial/webpack.config.js
+++ JavaScript Lvl 8 - Tooling e ES6 Modules/Aula 2 - Webpack/Aula 2 - Webpack tutorial/webpack.config.js
@@ -1,23 +1,23 @@
 const path = require('path'); // CommonJS
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: 'production', //Tipo de modo de produção
+  entry: './src/index.js', //Aqui criamos o arquivo de entrada seguindo essa estrutura
   output: {
-    path: path.resolve(__dirname, 'public', 'assets', 'js'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'public', 'assets', 'js'), //Utilizamos o path para resolver o caminho de saida do diretorio independente do sistema operacional
+    filename: 'bundle.js' //Este será o nome do arquivo que tera todas as modificações do webpack
   },
   module: {
-    rules: [{
-      exclude: /node_modules/,
-      test: /\.js$/,
-      use: {
+    rules: [{ //Em rules definimos as regras do modulo
+      exclude: /node_modules/, //Está regra impedira que o webpack analise a pasta do node_modules
+      test: /\.js$/, //Aqui definimos o tipo do arquivo que o webpack analisará apara adicionar ao bundle.js
+      use: { //Aqui definimos o que o modulo vai usar
         loader: 'babel-loader',
         options: {
           presets: ['@babel/env']
-        }
+        } //babel-loader e @babel/env que adicionamos no npm
       }
     }]
   },
-  devtool: 'source-map'
+  devtool: 'source-map' //Aqui temos uma dependencia para gerenciar possiveis erros na aplicação identificando o diretorio do arquivo onde está o erro
 };
