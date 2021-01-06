c5a774b7bb3afa477ea7f713fc24a0dc0e80f3d2
Isaac Moura de Oliveira <isaacestudo1999@gmail.com>
2021-01-06 08:12:22 -0500

    ...

---
+++ JavaScript Lvl 8 - Tooling e ES6 Modules/Aula 7 - Core-js e regenerator-runtime/modelo-webpack/src/main.js
@@ -0,0 +1,8 @@
+import 'core-js/stable'; //Utilizandos esse import para os navegadores antigos rodarem promises e async await
+import 'regenerator-runtime/runtime'; //Utilizando esse import para navegadores antigos rodarem promises e async await
+
+import executa from './modules/promises';
+
+import './assets/css/style.css';
+
+executa();
